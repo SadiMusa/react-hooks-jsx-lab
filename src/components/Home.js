5cf1a7c2f2d4a06df0e6b4b6130dda20913a47c5
Sadi Musa <sadimusa00@gmail.com>
2024-04-28 00:11:45 +0300

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
+  
   return <div>Home</div>;
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function NavBar() {
-  // update the JSX being returned!
+ !
   return <nav>NavBar</nav>;
 }
 
